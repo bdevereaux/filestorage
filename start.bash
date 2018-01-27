29722f20c9d572a35b19a9684d9906500a620b5d
bdevereaux3 <brandon.devereaux@commissionsinc.com>
2018-01-27 17:36:20 -0500

    added mode 1

--- start.bash
+++ start.bash
@@ -1,1 +1,1 @@
-./ethdcrminer64 -epool eth-us-east1.nanopool.org:9999 -ewal 0x5a83c0c286c5074f35927e5f3ac50c02b0ebdfff.hiveminer02/optional -epsw x
+./ethdcrminer64 -epool eth-us-east1.nanopool.org:9999 -ewal 0x5a83c0c286c5074f35927e5f3ac50c02b0ebdfff.hiveminer02/optional -epsw x -mode 1

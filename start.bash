b7abc6894ca7e5365afac9bb8df517d35c43da10
bdevereaux3 <brandon.devereaux@commissionsinc.com>
2018-01-27 17:54:52 -0500

    env vars

--- start.bash
+++ start.bash
@@ -1,3 +1,7 @@
-env LLVM_BIN=/opt/amdgpu-pro/bin /opt/amdgpu-pro/bin/clinfo
+export GPU_FORCE_64BIT_PTR 0
+export GPU_MAX_HEAP_SIZE 100
+export GPU_USE_SYNC_OBJECTS 1
+export GPU_MAX_ALLOC_PERCENT 100
+export GPU_SINGLE_ALLOC_PERCENT 100
 
 ./ethdcrminer64 -epool eth-us-east1.nanopool.org:9999 -ewal 0x5a83c0c286c5074f35927e5f3ac50c02b0ebdfff.hiveminer02/optional -epsw x -mode 1

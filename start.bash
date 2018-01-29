8b066a849a0b8ff496e9ea743c72adf9d4375af7
bdevereaux3 <brandon.devereaux@commissionsinc.com>
2018-01-28 23:13:15 -0500

    streamlined script

--- start.bash
+++ start.bash
@@ -1,7 +1,3 @@
-export GPU_FORCE_64BIT_PTR=0
-export GPU_MAX_HEAP_SIZE=100
-export GPU_USE_SYNC_OBJECTS=1
-export GPU_MAX_ALLOC_PERCENT=100
-export GPU_SINGLE_ALLOC_PERCENT=100
+export GPU_MAX_ALLOC_PERCENT=90
 
-./ethdcrminer64 -epool eth-us-east1.nanopool.org:9999 -ewal 0x5a83c0c286c5074f35927e5f3ac50c02b0ebdfff.hiveminer02/optional -epsw x -mode 1 -ftime 10
+./ethdcrminer64 -epool eth-us-east1.nanopool.org:9999 -ewal 0x5a83c0c286c5074f35927e5f3ac50c02b0ebdfff.hiveminer02/optional -epsw x -mode 1 -allpools 1 -tt 68

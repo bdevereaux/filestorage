64f210575f482615f0b52422dc178874c48b8e06
bdevereaux3 <brandon.devereaux@commissionsinc.com>
2018-01-29 00:19:45 -0500

    updated target temperatures for all cards, removed allpools flag

--- start.bash
+++ start.bash
@@ -1,3 +1,3 @@
 export GPU_MAX_ALLOC_PERCENT=90
 
-./ethdcrminer64 -epool eth-us-east1.nanopool.org:9999 -ewal 0x5a83c0c286c5074f35927e5f3ac50c02b0ebdfff.hiveminer02/optional -epsw x -mode 1 -allpools 1 -tt 68
+./ethdcrminer64 -epool eth-us-east1.nanopool.org:9999 -ewal 0x5a83c0c286c5074f35927e5f3ac50c02b0ebdfff.hiveminer02/optional -epsw x -mode 1 -tt 68,80,80

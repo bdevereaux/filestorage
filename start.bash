645da8c19f6877c55753f19ab5eba36820c328d1
bdevereaux3 <brandon.devereaux@commissionsinc.com>
2018-01-30 14:57:02 -0500

    creating hiveminer03 branch

--- start.bash
+++ start.bash
@@ -1,3 +1,3 @@
 export GPU_MAX_ALLOC_PERCENT=100
 
-./ethdcrminer64 -epool eth-us-east1.nanopool.org:9999 -ewal 0xFAbB342Da8D325a381122D9F6954BA5A4Ae0210f.hiveminer02/optional -epsw x -mode 1 -ttli 95 -tt 88 -fanmin 50
+./ethdcrminer64 -epool eth-us-east1.nanopool.org:9999 -ewal 0x5a83c0c286c5074f35927e5f3ac50c02b0ebdfff.hiveminer02/optional -epsw x -mode 1 -ttli 95 -tt 88 -fanmin 50

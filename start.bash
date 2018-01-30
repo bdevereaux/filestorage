cb641e6bd06e8a306a26312b0187df5fafee7898
bdevereaux3 <brandon.devereaux@commissionsinc.com>
2018-01-29 21:14:07 -0500

    updated target temperatures and max temperatures

--- start.bash
+++ start.bash
@@ -1,3 +1,3 @@
-export GPU_MAX_ALLOC_PERCENT=90
+export GPU_MAX_ALLOC_PERCENT=100
 
-./ethdcrminer64 -epool eth-us-east1.nanopool.org:9999 -ewal 0x5a83c0c286c5074f35927e5f3ac50c02b0ebdfff.hiveminer02/optional -epsw x -mode 1 -tt 68,80,80 -fanmin 50
+./ethdcrminer64 -epool eth-us-east1.nanopool.org:9999 -ewal 0x5a83c0c286c5074f35927e5f3ac50c02b0ebdfff.hiveminer02/optional -epsw x -mode 1 -ttli 95 -tt 88 -fanmin 50

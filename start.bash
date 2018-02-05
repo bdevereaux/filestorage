1f534982155886a37ff5b0896160c89b71bbc52e
bdevereaux3 <brandon.devereaux@commissionsinc.com>
2018-02-05 01:11:53 -0500

    removed sudo and commands

--- start.bash
+++ start.bash
@@ -1,29 +1,7 @@
 export GPU_MAX_ALLOC_PERCENT=100
 
-# echo '#!/bin/bash' > /home/miner/autostart.sh
-# echo 'if [ "x$1" = "x" -o "x$1" = "xnone" ]; then' >> /home/miner/autostart.sh
-# echo '    DELAY=$DEFAULT_DELAY' >> /home/miner/autostart.sh
-# echo 'else' >> /home/miner/autostart.sh
-# echo '    DELAY=$1' >> /home/miner/autostart.sh
-# echo 'fi' >> /home/miner/autostart.sh
-# echo 'sleep $DELAY' >> /home/miner/autostart.sh
-# echo 'cd /home/miner/minerscripts/filestorage' >> /home/miner/autostart.sh
-# echo "git checkout hiveminer04" >> /home/miner/autostart.sh
-# echo 'git pull' >> /home/miner/autostart.sh
-# echo 'cd /usr/local/claymore' >> /home/miner/autostart.sh
-# echo 'sudo cp /home/miner/minerscripts/filestorage/* ./' >> /home/miner/autostart.sh # copy all our files into our target directory
-# echo 'sudo chown root:root start.bash' >> /home/miner/autostart.sh
-# echo 'sudo chmod +x start.bash' >> /home/miner/autostart.sh
-
-# echo 'sudo apt-get install -y beep' >> /home/miner/autostart.sh
-# echo 'if ping -c 1 google.com >> /dev/null 2>&1; then' >> /home/miner/autostart.sh
-# echo '    su miner -c "screen -dmS ethm ./start.bash"' >> /home/miner/autostart.sh # starts the miner
-# echo 'else' >> /home/miner/autostart.sh
-# echo '    sudo modprobe pcspkr && beep && beep && beep && sudo shutdown -h now' >> /home/miner/autostart.sh
-# echo 'fi' >> /home/miner/autostart.sh
-
 if ping -c 1 google.com >> /dev/null 2>&1; then
-	touch /home/miner/test.sh
+	touch /root/test.sh
 	beep
 	beep
 	beep

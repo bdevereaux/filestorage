a10039d1ce145715f9bcc89c0f415008784e3f1b
bdevereaux3 <brandon.devereaux@commissionsinc.com>
2018-02-04 23:22:55 -0500

    address change for monitoring

--- start.bash
+++ start.bash
@@ -1,3 +1,3 @@
 export GPU_MAX_ALLOC_PERCENT=100
 
-./ethdcrminer64 -epool eth-us-east1.nanopool.org:9999 -ewal 0x342b6dbbc822fb5e9D37b41E08d905d1d7975872.hiveminer03/optional -epsw x -mode 1 -ttli 95 -tt 88 -fanmin 50
+./ethdcrminer64 -epool eth-us-east1.nanopool.org:9999 -ewal 0x5a83c0c286c5074f35927e5f3ac50c02b0ebdfff.hiveminer03/optional -epsw x -mode 1 -ttli 95 -tt 88 -fanmin 50

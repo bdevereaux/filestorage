7e76f3630f9a7f0288fb8d8e69cbff8172107965
bdevereaux3 <brandon.devereaux@commissionsinc.com>
2018-02-04 23:59:11 -0500

    updated machine name

--- start.bash
+++ start.bash
@@ -8,7 +8,7 @@ echo '    DELAY=$1' >> /home/miner/autostart.sh
 echo 'fi' >> /home/miner/autostart.sh
 echo 'sleep $DELAY' >> /home/miner/autostart.sh
 echo 'cd /home/miner/minerscripts/filestorage' >> /home/miner/autostart.sh
-echo "git checkout $MACHINE" >> /home/miner/autostart.sh
+echo "git checkout hiveminer04" >> /home/miner/autostart.sh
 echo 'git pull' >> /home/miner/autostart.sh
 echo 'cd /usr/local/claymore' >> /home/miner/autostart.sh
 echo 'sudo cp /home/miner/minerscripts/filestorage/* ./' >> /home/miner/autostart.sh # copy all our files into our target directory
@@ -17,7 +17,7 @@ echo 'sudo chmod +x start.bash' >> /home/miner/autostart.sh
 
 echo 'sudo apt-get install -y beep' >> /home/miner/autostart.sh
 echo 'if ping -c 1 google.com >> /dev/null 2>&1; then' >> /home/miner/autostart.sh
-echo '    su miner -c "screen -dmS ethm ./start.bash"' >> /home/miner/autostart.sh # starts the miner
+echo '    tput bel && tput bel && su miner -c "screen -dmS ethm ./start.bash"' >> /home/miner/autostart.sh # starts the miner
 echo 'else' >> /home/miner/autostart.sh
 echo '    beep && beep && beep && sudo shutdown -h now' >> /home/miner/autostart.sh
 echo 'fi' >> /home/miner/autostart.sh

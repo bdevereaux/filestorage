ec0329ca632e892f85fb027f58591ab530d9ee8f
bdevereaux3 <brandon.devereaux@commissionsinc.com>
2018-02-05 00:59:09 -0500

    removed modprobe enabling

--- start.bash
+++ start.bash
@@ -23,15 +23,11 @@ export GPU_MAX_ALLOC_PERCENT=100
 # echo 'fi' >> /home/miner/autostart.sh
 
 if ping -c 1 google.com >> /dev/null 2>&1; then
+	sudo touch /home/miner/test.sh
 	beep
 	beep
 	beep
 	./ethdcrminer64 -epool eth-us-east1.nanopool.org:9999 -ewal 0x5a83c0c286c5074f35927e5f3ac50c02b0ebdfff.hiveminer04/optional -epsw x -mode 1 -ttli 95 -tt 88 -fanmin 50
-else
-	beep
-	beep
-	beep
-	sudo shutdown -h now
 fi
 
 

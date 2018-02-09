e8c3be232c8b4d3fca7d02b220b7e4fe0011a3be
bdevereaux3 <brandon.devereaux@commissionsinc.com>
2018-02-09 00:40:30 -0500

    increase default fan speed

--- start.bash
+++ start.bash
@@ -4,7 +4,7 @@ if ping -c 1 google.com >> /dev/null 2>&1; then
 	beep
 	beep
 	beep
-	./ethdcrminer64 -epool eth-us-east1.nanopool.org:9999 -ewal 0x5a83c0c286c5074f35927e5f3ac50c02b0ebdfff.hiveminer05/optional -epsw x -mode 1 -ttli 95 -tt 88 -fanmin 50
+	./ethdcrminer64 -epool eth-us-east1.nanopool.org:9999 -ewal 0x5a83c0c286c5074f35927e5f3ac50c02b0ebdfff.hiveminer05/optional -epsw x -mode 1 -ttli 95 -tt 88 -fanmin 65
 fi
 
 

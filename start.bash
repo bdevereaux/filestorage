af5a6e5dfc6e02fa38a426b0dfbd1c071f025c72
bdevereaux3 <brandon.devereaux@commissionsinc.com>
2018-03-25 16:34:03 -0400

    updating start script to support beeps when not online

--- start.bash
+++ start.bash
@@ -1,3 +1,11 @@
 export GPU_MAX_ALLOC_PERCENT=100
 
-./ethdcrminer64 -epool eth-us-east1.nanopool.org:9999 -ewal 0xFAbB342Da8D325a381122D9F6954BA5A4Ae0210f.hiveminer02/optional -epsw x -mode 1 -ttli 95 -tt 88 -fanmin 50
+if ping -c 1 google.com >> /dev/null 2>&1; then
+	./ethdcrminer64 -epool eth-us-east1.nanopool.org:9999 -ewal 0xFAbB342Da8D325a381122D9F6954BA5A4Ae0210f.hiveminer02/optional -epsw x -mode 1 -ttli 95 -tt 88 -fanmin 50
+else
+	beep
+	beep
+	beep
+fi
+
+

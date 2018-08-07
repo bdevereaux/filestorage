1bbff3347e06a156219073f478b65c87819baeaf
bdevereaux3 <brandon.devereaux@commissionsinc.com>
2018-08-07 08:19:56 -0400

    removed unnecessary copying of startup script

--- start.bash
+++ start.bash
@@ -1,7 +1,22 @@
 export GPU_MAX_ALLOC_PERCENT=100
 
+cd /home/miner
+mkdir temp && cd temp
+wget https://github.com/nanopool/Claymore-Dual-Miner/releases/download/v11.8/Claymore.s.Dual.Ethereum.Decred_Siacoin_Lbry_Pascal_Blake2s_Keccak.AMD.NVIDIA.GPU.Miner.v11.8.-.LINUX.tar.gz
+tar -xzvf Claymore.s.Dual.Ethereum.Decred_Siacoin_Lbry_Pascal_Blake2s_Keccak.AMD.NVIDIA.GPU.Miner.v11.8.-.LINUX.tar.gz
+cd Claymore\'s\ Dual\ Ethereum+Decred_Siacoin_Lbry_Pascal_Blake2s_Keccak\ AMD+NVIDIA\ GPU\ Miner\ v11.8\ -\ LINUX/
+sudo cp ./* /usr/local/claymore
+cd /usr/local/claymore
+sudo chown root:root ethdcrminer64
+sudo chmod 755 ethdcrminer64
+sudo chmod u+s ethdcrminer64
+# sudo cp ~/minerscripts/filestorage/* ./
+# sudo chmod +x start.bash
+# sudo chown root:root start.bash
+rm -rf /home/miner/temp
+
 if ping -c 1 google.com >> /dev/null 2>&1; then
-	./ethdcrminer64 -epool eth-us-east1.nanopool.org:9999 -ewal 0xBbBcb9adDDe749617602565568b1c5f2dBa83999.hiveminer05/hiveminers@gmail.com -epsw x -mode 1 -ttli 95 -tt 85 -fanmin 50 -ftime 10
+	./ethdcrminer64 -epool eth-us-east1.nanopool.org:9999 -ewal 0xFAbB342Da8D325a381122D9F6954BA5A4Ae0210f.hiveminer02/hiveminers@gmail.com -epsw x -mode 1 -ttli 95 -tt 88 -fanmin 50 -ftime 10
 else
 	beep
 	beep

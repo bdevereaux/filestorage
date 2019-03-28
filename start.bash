47eb302d555a93e0c3893a8d191d742f682f1410
bdevereaux <bdevereaux3@gmail.com>
2019-03-27 21:35:57 -0400

    adjust miner intensity

--- start.bash
+++ start.bash
@@ -4,7 +4,7 @@ if ping -c 1 google.com >> /dev/null 2>&1; then
 	beep
 	beep
 	beep
-	./ethdcrminer64 -epool eth-us-east1.nanopool.org:9999 -ewal 0xe375EE7850c18edF7c71100C4B698014904101e6.hiveminer04/hiveminers@gmail.com -epsw x -mode 1 -ttli 95 -tt 88 -fanmin 70 -minspeed 30 -ethi 6
+	./ethdcrminer64 -epool eth-us-east1.nanopool.org:9999 -ewal 0xe375EE7850c18edF7c71100C4B698014904101e6.hiveminer04/hiveminers@gmail.com -epsw x -mode 1 -ttli 95 -tt 88 -fanmin 70 -minspeed 30 -ethi 2,0
 fi
 
 

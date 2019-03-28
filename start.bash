2bbd1b0255779ac4e93f5dd9b69bf8f1851b4938
bdevereaux3 <brandon.devereaux@commissionsinc.com>
2019-03-27 20:31:13 -0400

    updated email

--- start.bash
+++ start.bash
@@ -4,7 +4,7 @@ if ping -c 1 google.com >> /dev/null 2>&1; then
 	beep
 	beep
 	beep
-	./ethdcrminer64 -epool eth-us-east1.nanopool.org:9999 -ewal 0xe375EE7850c18edF7c71100C4B698014904101e6.hiveminer04/optional -epsw x -mode 1 -ttli 95 -tt 88 -fanmin 50
+	./ethdcrminer64 -epool eth-us-east1.nanopool.org:9999 -ewal 0xe375EE7850c18edF7c71100C4B698014904101e6.hiveminer04/hiveminers@gmail.com -epsw x -mode 1 -ttli 95 -tt 88 -fanmin 50
 fi
 
 
